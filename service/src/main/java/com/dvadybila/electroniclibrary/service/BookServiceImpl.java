9bbbe4bc86407ae6d850d067bfc1144ebf130f33
Stepan Tytseiko <stepantytseyko@gmail.com>
2017-10-17 15:50:09 +0300

    Created basic structure of electronic library web-site

---
+++ service/src/main/java/com/dvadybila/electroniclibrary/service/BookServiceImpl.java
@@ -0,0 +1,48 @@
+package com.dvadybila.electroniclibrary.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.dvadybila.electroniclibrary.dao.BookDao;
+import com.dvadybila.electroniclibrary.model.Book;
+
+@Service
+public class BookServiceImpl implements BookService {
+    private BookDao bookDao;
+
+    public void setBookDao(BookDao bookDao) {
+        this.bookDao = bookDao;
+    }
+
+    @Override
+    @Transactional
+    public void addBook(Book book) {
+        this.bookDao.addBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void updateBook(Book book) {
+        this.bookDao.updateBook(book);
+    }
+
+    @Override
+    @Transactional
+    public void removeBook(int id) {
+        this.bookDao.removeBook(id);
+    }
+
+    @Override
+    @Transactional
+    public Book getBookById(int id) {
+        return this.bookDao.getBookById(id);
+    }
+
+    @Override
+    @Transactional
+    public List<Book> listBooks() {
+        return this.bookDao.listBooks();
+    }
+}
---
+++ web/src/main/webapp/index.jsp
@@ -0,0 +1,13 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title>Electronic Library</title>
+</head>
+<body style="text-align: center;">
+<h3>401-EK presents</h3>
+<br/>
+<a href="<c:url value="/books"/>" >Books list</a> <!-- target="_blank" -->
+<br/>
+</body>
+</html>
